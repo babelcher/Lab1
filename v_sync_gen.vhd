c325d28fb81af4096bdc4df35e559b6e1557a9a9
Brandon Belcher <c15brandon.belcher@usafa.edu>
2014-01-31 13:09:00 -0700

    First commit of all the code that is given on the website. No changes yet.

---
+++ v_sync_gen.vhd
@@ -0,0 +1,48 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    11:06:28 01/29/2014 
+-- Design Name: 
+-- Module Name:    v_sync_gen - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity v_sync_gen is
+    Port ( clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           h_blank : in  STD_LOGIC;
+           v_sync : out  STD_LOGIC;
+           blank : out  STD_LOGIC;
+           completed : out  STD_LOGIC;
+           row : out  unsigned(10 downto 0);
+end v_sync_gen;
+
+architecture Behavioral of v_sync_gen is
+
+begin
+
+
+end Behavioral;
+
